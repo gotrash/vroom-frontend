1af59983c78bbd48e47d3c645d71707e8d0e508d
Alex Scott <codetheorist@outlook.com>
2024-09-15 13:34:38 +0100

    fix: API connection details

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  host: 'http://localhost',
-  port: '3000',
+  host: 'https://vroom-api.gotrash.co.uk',
+  port: '443',
   maxTaskNumber: 100,
   overpassEndpoint: 'https://overpass-api.de/api/interpreter'
 };
